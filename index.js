540d82fdf16c0089eac0f80c4a2e6f4862326fe8
abhimanyusoni <abhimanyushady@gmail.com>
2023-02-23 16:20:19 +0530

    Socket

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server("https://socket-mauve.vercel.app", {
   cors: {
-    origin: "https://whatsapp-frontend-kohl.vercel.app",
+    origin: "*",
   },
 });
 
